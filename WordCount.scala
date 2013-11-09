12776c2ff6d344489229e342650cd7fb12b4224e
Manny <mjl288@scarletmail.rutgers.edu>
2013-11-09 14:58:26 -0500

    tokenized input

--- WordCount.scala
+++ WordCount.scala
@@ -4,7 +4,21 @@ import scala.collection.mutable.ArrayBuffer
 
 object WordCount{
   def main(args: Array[String]){
-    
+    println("Enter sentences for concordance output: ")
+    val text = readLine
+    parseText(text)
+  }
 
+
+  def parseText(text: String){
+    
+    val st = new StringTokenizer(text)
+    var word = ""
+
+    while(st.hasMoreTokens()){
+      word = st.nextToken()
+      println(word)
+    }
+    println("finished parsing")
   }
 }
